919085a4745c1f563d1eb77ce64b5f2f4ffa5a93
nglszs <1458583930@qq.com>
2016-04-25 13:01:27 +0800

    Initial Commit

---
+++ BCCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BCCamera
+//
+//  Created by Jack on 16/4/25.
+//  Copyright © 2016年 毕研超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
